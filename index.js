a699c85ac532679890efef4fa27d933e3d6d9a2f
Daniel Succar <danielsuccar@icloud.com>
2019-12-16 20:41:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,6 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   return("I can\'t hear you!")
   }
+function sayHiToGrandma(string) {
+  if string = .uppercase
+  return("YES INDEED")
