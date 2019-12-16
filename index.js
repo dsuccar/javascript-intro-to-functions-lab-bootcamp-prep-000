3f8ed1e1a4b194543bac9a7673f93d824702a33f
Daniel Succar <danielsuccar@icloud.com>
2019-12-16 21:04:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,16 +18,16 @@ function sayHiToGrandma(string){
   var uppercase = "HELLO"
   var lowercase = "hello"
 
-  if (string = lowercase){
-  if (string = string.toLowerCase()){
+  if (string === lowercase){
+  if (string === string.toLowerCase()){
     return "I can't hear you!"
   }
-  else if (string = uppercase){
+  else if (string === uppercase){
   }
-  else if (string = string.toUpperCase()){
+  else if (string === string.toUpperCase()){
     return "YES INDEED!"
   }
-  else if (string = "I love you, Grandma."){
+  else if (string === "I love you, Grandma."){
     return "I love you, too."
   }
 }
